57ca1da58c8bd392394cf5597ba0225ff1f7f1e6
Ranjani Renganathan <ranjanivsp@gmail.com>
2022-04-29 19:18:43 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
+import sys
+from crawl import crawl_page
+from report import print_report
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("no website provided")
+        exit(1)
+    base_url = sys.argv[1]
+    pages = {}
+    pages = crawl_page(base_url, base_url, pages)
+    print_report(pages)
+
+
+if __name__ == "__main__":
+    main()
 
